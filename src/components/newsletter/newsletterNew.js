949a716ededffb67f2b424ea2a0b8b595832c33b
TCMooney <tcmooney88@gmail.com>
2019-01-27 16:04:29 -0500

    built newsletter new form

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import SigninForm from '../auth/signinForm';
+import  NewNewsletterForm from './newsletterNewForm'
 
 class NewNewsletter extends Component {
 
@@ -11,7 +11,7 @@ class NewNewsletter extends Component {
    render() {
        return (
            <div className="newsletter-new">
-               <SigninForm onSubmit={(event) => this.onSubmit(event)}/>
+               <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)}/>
            </div>
        );
    }
