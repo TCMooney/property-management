087244ad3a5634ede44cd0da3ae7d37d5bfa2c7b
TCMooney <tcmooney88@gmail.com>
2019-01-31 15:36:43 -0500

    started building requests feature

---
+++ src/components/requests/requestGrid.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class RequestsGrid extends Component {
+   render() {
+       return (
+           <div className="requests-grid">
+               Hey There
+           </div>
+       );
+   }
+}
+
+export default RequestsGrid;
