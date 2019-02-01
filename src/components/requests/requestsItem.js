eff0227805c3ce8738671cbf04fb346b0e5fdf6f
TCMooney <tcmooney88@gmail.com>
2019-02-01 11:29:30 -0500

    built a functional react icon component utilizing font awesome

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,11 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+import Icon from '../icon';
 
 export default class RequestsItem extends Component {
-   render() {
-       return (
-           <div className="requests-item">
-               item goes here
-           </div>
-       );
-   }
+    render() {
+        return (
+            <div className="requests-item">
+                <div>
+                    <Icon icon='fas fa-exclamation-triangle'/>
+                </div>
+            </div>
+        );
+    }
 }
