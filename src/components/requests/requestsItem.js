2c79e5d4cf281895e74147a137577677ab65d42f
TCMooney <tcmooney88@gmail.com>
2019-02-01 11:55:01 -0500

    JSX for requestsItem

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 
 import Icon from '../icon';
+import Button from '../button';
 
 export default class RequestsItem extends Component {
     render() {
         return (
             <div className="requests-item">
-                <div>
-                    <Icon icon='fas fa-exclamation-triangle'/>
+                <Icon className="requests-item__icon" icon='fas fa-exclamation-triangle'/>
+                <div className="requests-item__title">
+                    Yo my sink stinks
                 </div>
+                <div className="requests-item__tenant-unit">
+                    Tom - Unit 666
+                </div>
+                <Icon className="requests-item__arrow" icon="fas fa-sort-down"/>
+                <div className="requests-item__date">
+                    06/26/1988
+                </div>
+                <Button className="requests-item__move" icon="fas fa-wrench" callback={console.log('change request item status')}/>
             </div>
         );
     }
