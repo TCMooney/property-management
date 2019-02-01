41d008d2f25ab083ca32d84641f0f62e6d570a76
TCMooney <tcmooney88@gmail.com>
2019-01-31 21:16:10 -0500

    changed styles based on selectedrequesttype in store and fixed the payload bug on the changeselecetdrequesttype

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,11 +1,9 @@
-import {
-    CHANGE_SELECTED_REQUEST_TYPE    
-} from './types';
+import { CHANGE_SELECETED_REQUEST_TYPE } from './types';
 
 export function changeSelectedRequestType(boxType) {
     return (
         {
-            type: CHANGE_SELECTED_REQUEST_TYPE,
+            type: CHANGE_SELECETED_REQUEST_TYPE,
             payload: boxType
         }
     )
--- src/reducers/requestsReducer.js
+++ src/reducers/requestsReducer.js
@@ -1,19 +1,19 @@
 import {
-    CHANGE_SELECETED_REQUESTS_TYPE
+    CHANGE_SELECETED_REQUEST_TYPE
 } from '../actions/types';
 
 const INITIAL_STATE = {
     requests: [],
-    selectedRequests: 'pending'
+    selectedRequestType: 'pending'
 }
 
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
-        case CHANGE_SELECETED_REQUESTS_TYPE:
+        case CHANGE_SELECETED_REQUEST_TYPE:
             const boxType = action.payload
             return {
                 ...state,
-                selectedRequests: boxType
+                selectedRequestType: boxType
             }
         default: return state;
     }
