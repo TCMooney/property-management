bb6eb3a860ef5bb6ce9c79a5813c2cd36e571f43
TCMooney <tcmooney88@gmail.com>
2019-02-03 14:38:09 -0500

    build create new request action creator with redux-thunk middleware

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,5 +1,8 @@
 import { CHANGE_SELECETED_REQUEST_TYPE } from './types';
 
+import axios from 'axios';
+import { ROOT_URL } from '../config';
+
 export function changeSelectedRequestType(boxType) {
     return (
         {
@@ -8,3 +11,16 @@ export function changeSelectedRequestType(boxType) {
         }
     )
 };
+
+export function createNewRequests(newRequest, success) {
+    return function() {
+        axios.post(`${ROOT_URL}/requests/new`)
+            then(response => {
+                console.log(response.data);
+                success();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
