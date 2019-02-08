14d56d827565c10b79f92a4e06dad13aae4ec379
TCMooney <tcmooney88@gmail.com>
2019-02-08 16:50:17 -0500

    hooked up layout to mapstatetoprops

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { Header, HeaderBar } from './header';
 
@@ -13,5 +14,11 @@ class Layout extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    return state;
+}
+
+Layout = connect(mapStateToProps)(Layout);
 
 export default Layout;
