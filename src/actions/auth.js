de44f270cadcd274289b8d68e3df71d84ebc48a9
TCMooney <tcmooney88@gmail.com>
2019-02-09 10:02:31 -0500

    built requireadmin component and used it on the new newsletter button

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -9,6 +9,13 @@ import {
 
 
 export function signUp(fields, success) {
+    // var newFields = {
+    //     admin: true
+    // }
+    // newFields = {
+    //     ...fields,
+    //     ...newFields
+    // }
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
             .then(response => {
